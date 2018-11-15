b3da99d484798ce7d2f90cf19fbaaf71b0e489f0
Mike Shaw <mikeshaw@Mikes-MBP-2.countryscape.org>
2018-11-15 10:32:09 +0000

    Overdue backup

--- template.php
+++ template.php
@@ -13,3 +13,30 @@ function connectingnature_preprocess_html(&$vars) {
     $vars['classes_array'][] = drupal_clean_css_identifier($alias);
   }
 }
+
+
+
+function connectingnature_form_alter(&$form, &$form_state, $form_id) {
+  // Check when the right form is passed
+
+   if($form_id == "views_exposed_form"){
+    if (isset($form['combine'])) {
+            $form['combine']['#attributes'] = array('placeholder' => array(t('Enter search terms:')));
+    }
+  }
+
+}
+
+
+
+
+
+function  connectingnature_css_alter(&$css) {
+ $exclude = array(
+//'sites/all/libraries/leaflet/leaflet.css' => FALSE ,
+'sites/all/modules/flexslider/assets/css/flexslider_img.css' => FALSE);
+$css = array_diff_key($css, $exclude);
+
+}
+
+
